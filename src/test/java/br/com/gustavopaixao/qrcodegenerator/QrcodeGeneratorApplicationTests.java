f605af25b6ddef3e925668bdaa695ac61a37ddb3
Guh-paixao <gustavopaixao.dev@gmail.com>
2025-04-28 14:19:30 -0300

    initialize project with Spring Boot and Maven wrapper

---
+++ src/test/java/br/com/gustavopaixao/qrcodegenerator/QrcodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavopaixao.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
